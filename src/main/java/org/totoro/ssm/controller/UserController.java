f82c67fbb28cbb7811b094bec70f41604b410efa
Jiangpeizhao <Jiangpeizhao@gmail.com>
2015-12-15 18:51:31 +0800

    有问题，暂存

---
+++ src/main/java/org/totoro/ssm/controller/UserController.java
@@ -0,0 +1,26 @@
+package org.totoro.ssm.controller;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.totoro.ssm.pojo.User;
+import org.totoro.ssm.service.IUserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@Resource
+	private IUserService userService;
+	
+	@RequestMapping("/showUser")
+	public String toIndex(HttpServletRequest request,Model model){
+		int userId = Integer.parseInt(request.getParameter("id"));
+		User user = this.userService.getUserById(userId);
+		model.addAttribute("user", user);
+		return "showUser";
+	}
+}
+
